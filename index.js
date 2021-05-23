cc2d8db8d6f49e8f3767320e50f45d8a0cc13ff9
PargaTolano <parga.jose@outlook.com>
2021-05-23 18:10:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const app = express();
 app.use( express.static(path.join(__dirname, 'public')));
 
 app.listen( process.env.PORT || 3000, ()=>{
-    console.log('Aplicacion montada en el puerto 3000 soy una verga');
+    console.log('Aplicacion montada en el puerto 3000');
 } );
