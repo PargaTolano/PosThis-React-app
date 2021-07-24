550d496e7c874065f0a4f107a76a8fc036e9432a
PargaTolano <parga.jose@outlook.com>
2021-07-24 17:56:13 -0500

    static files served

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const path    = require("path");
-const express = require("express");
+const path    = require('path');
+const express = require('express');
 const app     = express();
 
+app.use( express.static( path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('*', (req, res) => {
     res.redirect('/');
 });
 
-app.listen( process.env.PORT || 3000, () => {
-  console.log("server started on port 5000");
+const port = process.env.PORT || 3001;
+app.listen( port, () => {
+    console.log(`posthis-node app mounted on port ${port}`);
 });
